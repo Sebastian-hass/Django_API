9642458bf7de1db85b83528c962195ffa65f3991
Sebastian <jesussebastianalonsoarias@gmail.com>
2025-06-19 13:43:22 +0200

    Final changes beafore render deployment

--- misitio/misitio/settings.py
+++ misitio/misitio/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j081ak6eop^l=dzk3@ck5-^x!#v005$mv1*mo1u9t0+&@7e@2x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]#, os.environ.get("RAILWAY_STATIC_URL", "")
-DEBUG = os.environ.get("DEBUG", "") != "False"
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 # Application definition
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
